e403f3be93e19f2bb6dc0b5f1c14e0cf38e80094
vovietmy <vovietmy12@gmail.com>
2023-10-28 16:30:52 +0700

    tich

--- Source.cpp
+++ Source.cpp
@@ -3,7 +3,10 @@
 using namespace std;
 int tong(int a, int b);
 int hieu(int a, int b);
-int tich(int a, int b);
+int tich(int a, int b){
+
+return a*b;
+}
 float thuong(int a, int b);
 int main()
 {
@@ -14,9 +17,9 @@ int main()
 	cout<<"Nhap so thu hai: ";
 	cin>>b;
 	cout<<a<<" + "<<b<<" = "<<tong(a+b)<<endl;
-	cout<<a<<" - "<<b<<" = "<<hieu(a+b)<<endl;
-	cout<<a<<" * "<<b<<" = "<<tich(a+b)<<endl;
-	cout<<a<<" / "<<b<<" = "<<thuong(a+b)<<endl;
+	cout<<a<<" - "<<b<<" = "<<hieu(a-b)<<endl;
+	cout<<a<<" * "<<b<<" = "<<tich(a*b)<<endl;
+	cout<<a<<" / "<<b<<" = "<<thuong(a/b)<<endl;
 	system("pause");
 	return 0;
 }
